407c731d87fe6471154457b3aef641a96cbb27d7
VitaliyShulik <you@example.com>
2020-02-26 17:33:11 +0600

    code refactoring

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+////// Module game building
+
+var table = buildGameTable(8, 8, 5);
+
 function buildGameTable(rows, cols, maxMines) {
-    let tableCols = {};
-    let table = {counterMines:0, counterFlags:0, amountCols: rows * cols, timeCounterIsStart: false};
+    let tableCells = {};
+    let table = {counterMines:0, counterFlags:0, amountCells: rows * cols, timeCounterIsStart: false};
     let gameTableElement = document.getElementById("gameTable");
     document.getElementById("amount-mines").innerHTML = maxMines;
     
@@ -17,16 +21,16 @@ function buildGameTable(rows, cols, maxMines) {
         
         // create row's cols
         for (let n = 0; n < cols; n++) {
-            let divCol = document.createElement("div");
-            divCol.setAttribute("class", "col");
-            divCol.addEventListener("click", clickOnCol);
-            divCol.addEventListener("contextmenu", rightClickOnCol, false);
-            let colId = "col-" + i + "-" + n;
-            divCol.setAttribute("id", colId);
-            divRow.appendChild(divCol);
+            let divCell = document.createElement("div");
+            divCell.setAttribute("class", "cell");
+            divCell.addEventListener("click", clickOnCell);
+            divCell.addEventListener("contextmenu", rightClickOnCell, false);
+            let cellId = "cell-" + i + "-" + n;
+            divCell.setAttribute("id", cellId);
+            divRow.appendChild(divCell);
 
             // adding col with properties to table object
-            Object.assign(tableCols, {[colId]: {
+            Object.assign(tableCells, {[cellId]: {
                  row: i,
                  col: n,
                  neighbors: setNeighbors(rows, cols, i, n),
@@ -37,24 +41,13 @@ function buildGameTable(rows, cols, maxMines) {
              }});
             }
     }
-    table.tableCols = tableCols;
+    table.tableCells = tableCells;
 
     while (table.counterMines != maxMines) {
         setMine(maxMines, table);
     }
     addNieghbors(table);
     return table
-}
-
-function setMine(maxMines, table) {
-    for (cols in table.tableCols){
-        let colObject = table.tableCols[cols];
-        if (table.counterMines < maxMines && Math.random() > 0.98 && !colObject.haveMine){
-            table.counterMines++;
-            colObject.haveMine = true;
-        }
-    } 
-     
 }
 
 function setNeighbors(rows, cols, i, n) {
@@ -162,10 +155,21 @@ function setNeighbors(rows, cols, i, n) {
     }
 }
 
+function setMine(maxMines, table) {
+    for (cell in table.tableCells){
+        let cellObject = table.tableCells[cell];
+        if (table.counterMines < maxMines && Math.random() > 0.98 && !cellObject.haveMine){
+            table.counterMines++;
+            cellObject.haveMine = true;
+        }
+    } 
+     
+}
+
 function addNieghbors(table) {
-    for (const x in table.tableCols){
+    for (const x in table.tableCells){
         let counterNierhbors = 0;
-        let xNeighbors = table.tableCols[x].neighbors;
+        let xNeighbors = table.tableCells[x].neighbors;
         for (const k in xNeighbors){
             let nowNeighbors = xNeighbors[k];
             let row = nowNeighbors.row;
@@ -178,7 +182,7 @@ function addNieghbors(table) {
             }
         
         }
-        Object.assign(table.tableCols[x], {amountNeighborsWithMine: counterNierhbors});
+        Object.assign(table.tableCells[x], {amountNeighborsWithMine: counterNierhbors});
     }
  
 }
@@ -190,54 +194,68 @@ function checkMine(row, col, table) {
 
 function findNeigbor(row, col, table) {
     let neighbor = "";
-    for (const k in table.tableCols){
-        if (table.tableCols[k].row == row && table.tableCols[k].col == col){
-            neighbor = table.tableCols[k];
+    for (const k in table.tableCells){
+        if (table.tableCells[k].row == row && table.tableCells[k].col == col){
+            neighbor = table.tableCells[k];
         }
     }
     return neighbor;
     
 }
 
-function clickOnCol(event){
+//////////
+
+///////// Module Actions
+
+// Left click
+
+function clickOnCell(event){
+    let cellId = event.target.id;
+    let cellElement = document.getElementById(cellId);
+
     if (!table.timeCounterIsStart){startTimer()}
-    let colId = event.target.id;
-    let colElement = document.getElementById(colId);
-    let haveMine = table.tableCols[colId].haveMine;
-    let amountNeighborsWithMine = table.tableCols[colId].amountNeighborsWithMine;
+
+    let haveMine = table.tableCells[cellId].haveMine;
+    let amountNeighborsWithMine = table.tableCells[cellId].amountNeighborsWithMine;
+    
     if (haveMine){
-        colElement.style.backgroundColor = "rgba(255, 0, 0, 0.6)";
-        colElement.style.backgroundImage = "url('./img/bomb.svg')";
-        table.tableCols[colId].isOpen = true;
+        cellElement.style.backgroundColor = "rgba(255, 0, 0, 0.6)";
+        cellElement.style.backgroundImage = "url('./img/bomb.svg')";
+        table.tableCells[cellId].isOpen = true;
         gameOver();      
     } else if(!haveMine && amountNeighborsWithMine > 0){
-        getNumberToCol(amountNeighborsWithMine, colElement, colId);
-        table.tableCols[colId].isOpen = true;
+        getNumberToCell(amountNeighborsWithMine, cellElement);
+        table.tableCells[cellId].isOpen = true;
     } else {
-        openEmptyCol(colElement, colId, table);
+        openEmptyCell(cellElement, cellId, table);
     }
-    table.counterFlags = checkAndAddNumberCollsWithFlag();
+
+    table.counterFlags = checkAndAddNumberCellsWithFlag();
     let counterFlagsSpan = document.getElementById("counter-flags");
     counterFlagsSpan.innerHTML = table.counterFlags;
 
-    let amountOpenedCols = checkAmountOpenedCols();
-    let amountCols = table.amountCols;
-    let amountClosedCols = amountCols - amountOpenedCols;
-    if (amountClosedCols == table.counterMines){
-        won()
-    }
+    checkamountOpenedCells()
 }
 
-function getNumberToCol(amountNeighborsWithMine, colElement, colId){
+function getNumberToCell(amountNeighborsWithMine, cellElement){
     let urlImg = "url('./img/" + amountNeighborsWithMine + ".png')"
-    colElement.style.backgroundSize = "cover";
-    colElement.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
-    colElement.style.backgroundImage = urlImg;
-    colElement.removeEventListener("contextmenu", rightClickOnCol, false);
+    cellElement.style.backgroundSize = "cover";
+    cellElement.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
+    cellElement.style.backgroundImage = urlImg;
+    cellElement.removeEventListener("contextmenu", rightClickOnCell, false);
 }
 
-function checkNeighborsWithNeighborsWithMine(table, colId) {
-    let neighbors = table.tableCols[colId].neighbors;
+function openEmptyCell(cellElement, cellId, table){
+    cellElement.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
+    table.tableCells[cellId].isOpen = true;
+    table.tableCells[cellId].haveFlag = false;
+    cellElement.style.backgroundImage = '';
+    cellElement.removeEventListener("contextmenu", rightClickOnCell, false);
+    checkNeighborsWithNeighborsWithMine(table, cellId);
+}
+
+function checkNeighborsWithNeighborsWithMine(table, cellId) {
+    let neighbors = table.tableCells[cellId].neighbors;
     for (neighbor in neighbors){
         if (neighbors[neighbor] !== ""){
             let row = neighbors[neighbor].row;
@@ -248,41 +266,39 @@ function checkNeighborsWithNeighborsWithMine(table, colId) {
 }
 
 function workOnNeighbor(row, col, table) {
-    let colId = "col-" + row + "-" + col;
-    let colElement = document.getElementById(colId);
+    let cellId = "cell-" + row + "-" + col;
+    let cellElement = document.getElementById(cellId);
     let neighbor = findNeigbor(row, col, table);
     let amountNeighborsWithMine = neighbor.amountNeighborsWithMine;
-    let isOpen = table.tableCols[colId].isOpen;
+    let isOpen = table.tableCells[cellId].isOpen;
     if (amountNeighborsWithMine != 0){
-        getNumberToCol(amountNeighborsWithMine, colElement);
-        table.tableCols[colId].isOpen = true;
-        table.tableCols[colId].haveFlag = false;
+        getNumberToCell(amountNeighborsWithMine, cellElement);
+        table.tableCells[cellId].isOpen = true;
+        table.tableCells[cellId].haveFlag = false;
     } else if (amountNeighborsWithMine == 0 && !isOpen){
-        openEmptyCol(colElement, colId, table);
+        openEmptyCell(cellElement, cellId, table);
     }
 }
-function openEmptyCol(colElement, colId, table){
-    colElement.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
-    table.tableCols[colId].isOpen = true;
-    table.tableCols[colId].haveFlag = false;
-    colElement.style.backgroundImage = '';
-    colElement.removeEventListener("contextmenu", rightClickOnCol, false);
-    checkNeighborsWithNeighborsWithMine(table, colId);
+
+function checkamountOpenedCells() {
+    let amountOpenedCells = 0;
+    for (cell in table.tableCells){
+        if (table.tableCells[cell].isOpen){
+            amountOpenedCells++;
+        }
+    }
+    let amountCells = table.amountCells;
+    let amountClosedCells = amountCells - amountOpenedCells;
+    if (amountClosedCells == table.counterMines){
+        won()
+    }
 }
-
-
-var table = buildGameTable(8, 8, 10);
 
 function restartGame() {
     removeGameTable();
-    table = buildGameTable(8, 8, 10);
+    table = buildGameTable(8, 8, 5);
     stopTimer();
-    let secondsElement = document.getElementById('seconds');
-    let minutesElement = document.getElementById('minutes');
-    let hoursElement = document.getElementById('hours');
-    secondsElement.innerHTML = "00";
-    minutesElement.innerHTML = "00";
-    hoursElement.innerHTML = "00";
+    resetTimeCounter();
 }
 
 function removeGameTable() {
@@ -297,55 +313,53 @@ function removeGameTable() {
 function gameOver() {
     let gameTable = document.getElementById('gameTable');
     gameTable.style.webkitFilter = "blur(.05em)";
-    for (x in table.tableCols){
-        let colId = x;
-        let haveMine = table.tableCols[x].haveMine;
-        let isOpen = table.tableCols[x].isOpen;
-        let colElement = document.getElementById(colId);
-        colElement.removeEventListener("click", clickOnCol);
-        colElement.removeEventListener("contextmenu", rightClickOnCol, false);
+    for (x in table.tableCells){
+        let cellId = x;
+        let haveMine = table.tableCells[x].haveMine;
+        let isOpen = table.tableCells[x].isOpen;
+        let cellElement = document.getElementById(cellId);
+        cellElement.removeEventListener("click", clickOnCell);
+        cellElement.removeEventListener("contextmenu", rightClickOnCell, false);
         if (haveMine && !isOpen){
-            colElement.style.backgroundImage = "url('./img/bomb.svg')";
-            colElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+            cellElement.style.backgroundImage = "url('./img/bomb.svg')";
+            cellElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
         } else if (!isOpen){
-            colElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+            cellElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
         }
     }
     stopTimer();
 }
 
 
-function rightClickOnCol(e) {
+function rightClickOnCell(e) {
     if (!table.timeCounterIsStart){startTimer()}
     e.preventDefault();
-    let colId = event.target.id;
-    let colElement = document.getElementById(colId);
+    let cellId = event.target.id;
+    let cellElement = document.getElementById(cellId);
     
-    let haveFlag = table.tableCols[colId].haveFlag;
+    let haveFlag = table.tableCells[cellId].haveFlag;
     if (!haveFlag && table.counterFlags < table.counterMines){
-        colElement.removeEventListener("click", clickOnCol);
-        colElement.style.backgroundSize = "cover";
-        colElement.style.backgroundImage = "url('./img/flag.png')";
-        table.tableCols[colId].haveFlag = true;
+        cellElement.removeEventListener("click", clickOnCell);
+        cellElement.style.backgroundSize = "cover";
+        cellElement.style.backgroundImage = "url('./img/flag.png')";
+        table.tableCells[cellId].haveFlag = true;
     } else if (haveFlag){
-        colElement.addEventListener("click", clickOnCol);
-        colElement.style.backgroundSize = "";
-        colElement.style.backgroundImage = "";
-        table.tableCols[colId].haveFlag = false;
+        cellElement.addEventListener("click", clickOnCell);
+        cellElement.style.backgroundSize = "";
+        cellElement.style.backgroundImage = "";
+        table.tableCells[cellId].haveFlag = false;
     }
-    table.counterFlags = checkAndAddNumberCollsWithFlag();
+    
+    table.counterFlags = checkAndAddNumberCellsWithFlag();
     let counterFlagsSpan = document.getElementById("counter-flags");
     counterFlagsSpan.innerHTML = table.counterFlags;
-    if (checkColsWithFlagsAndMines()){
-        won()
-    }
-
+    checkCellsWithFlagsAndMines()
 }
 
-function checkAndAddNumberCollsWithFlag() {
+function checkAndAddNumberCellsWithFlag() {
     let counterFlags = 0;
-    for (cols in table.tableCols){
-        if (table.tableCols[cols].haveFlag){
+    for (cell in table.tableCells){
+        if (table.tableCells[cell].haveFlag){
             counterFlags++;
         }
     }
@@ -353,58 +367,60 @@ function checkAndAddNumberCollsWithFlag() {
     return table.counterFlags
 }
 
-function checkAmountOpenedCols() {
-    let amountOpenedCols = 0;
-    for (cols in table.tableCols){
-        if (table.tableCols[cols].isOpen){
-            amountOpenedCols++;
-        }
-    }
-    return amountOpenedCols
-}
+
 
 function won() {
     let gameTable = document.getElementById('gameTable');
     gameTable.style.webkitFilter = "blur(.05em)";
-    for (x in table.tableCols){
-        let colId = x;
-        let haveMine = table.tableCols[x].haveMine;
-        let isOpen = table.tableCols[x].isOpen;
-        let colElement = document.getElementById(colId);
-        colElement.removeEventListener("click", clickOnCol);
-        colElement.removeEventListener("contextmenu", rightClickOnCol, false);
-        if (haveMine && !isOpen){
-            colElement.style.backgroundImage = "url('./img/bomb.svg')";
-            colElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-        } else if (!isOpen){
-            colElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-        }
+    for (x in table.tableCells){
+        openAllCells(x)
     }
     stopTimer();
 }
 
-function checkColsWithFlagsAndMines(){
-    let counterColsWithFlagAndMines = 0;
-    for (cols in table.tableCols){
-        if (table.tableCols[cols].haveFlag && table.tableCols[cols].haveMine){
-            counterColsWithFlagAndMines++
+function openAllCells(x) {
+    let cellId = x;
+        let haveMine = table.tableCells[x].haveMine;
+        let isOpen = table.tableCells[x].isOpen;
+        let cellElement = document.getElementById(cellId);
+        cellElement.removeEventListener("click", clickOnCell);
+        cellElement.removeEventListener("contextmenu", rightClickOnCell, false);
+        if (haveMine && !isOpen){
+            cellElement.style.backgroundImage = "url('./img/bomb.svg')";
+            cellElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+        } else if (!isOpen){
+            cellElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+        }
+}
+
+function checkCellsWithFlagsAndMines(){
+    let counterCellsWithFlagAndMines = 0;
+    for (cell in table.tableCells){
+        if (table.tableCells[cell].haveFlag && table.tableCells[cell].haveMine){
+            counterCellsWithFlagAndMines++
         }
     }
-    return counterColsWithFlagAndMines == table.counterMines
+    if (counterCellsWithFlagAndMines == table.counterMines){
+        won()
+    }
 }
+
+/////// Time counter module
 
 var interval; 
 
 function startTimer(){
     let timeCounter = 1;
+    let seconds, minutes, hours = 0;
+
     let secondsElement = document.getElementById('seconds');
     let minutesElement = document.getElementById('minutes');
     let hoursElement = document.getElementById('hours');
-    secondsElement.innerHTML = "00";
-    minutesElement.innerHTML = "00";
-    hoursElement.innerHTML = "00";
-    let seconds, minutes, hours = 0;
+
+    resetTimeCounter();
+
     table.timeCounterIsStart = true;
+
     interval = setInterval(() => {
         if (timeCounter < 60){
             seconds = timeCounter;
@@ -448,3 +464,14 @@ function stopTimer() {
     clearInterval(interval);
     table.timeCounterIsStart = false;
 }
+
+function resetTimeCounter() {
+    let secondsElement = document.getElementById('seconds');
+    let minutesElement = document.getElementById('minutes');
+    let hoursElement = document.getElementById('hours');
+    secondsElement.innerHTML = "00";
+    minutesElement.innerHTML = "00";
+    hoursElement.innerHTML = "00";
+}
+
+////////
