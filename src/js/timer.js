04adf7cde83e2c74369878ef23ca5d647b934933
Peter Dematté <PitPik@users.noreply.github.com>
2023-01-29 11:20:40 +0100

    Update timer.js
    - remove repetitive code (document.getElementById, secondsElement.innerHTML =, ...)
    - create setTime() to reduce code repetition
    - clearInterval inside startTimer to avoid accidental multiple intervals
    - use .textContent instead of .innerHTML for performance (and because the numbers are not HTML anyhow but text only)
    - Simplify code (removed ~60% of the code)

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,56 +1,25 @@
 import { table } from "./app.js";
 
-var interval; 
+const secondsElement = document.getElementById('seconds');
+const minutesElement = document.getElementById('minutes');
+const hoursElement = document.getElementById('hours');
 
-export function startTimer(){
-    let timeCounter = 1;
-    let seconds, minutes, hours = 0;
+let interval = 0;
+let timeCounter = 0;
 
-    let secondsElement = document.getElementById('seconds');
-    let minutesElement = document.getElementById('minutes');
-    let hoursElement = document.getElementById('hours');
+function setTime(seconds) {
+    secondsElement.textContent = `0${seconds % 60}`.slice(-2);
+    minutesElement.textContent = `0${Math.floor(seconds / 60) % 60}`.slice(-2);
+    hoursElement.textContent = `0${Math.floor(seconds / 60 / 60)}`.slice(-2);
+}
 
+export function startTimer() {
     resetTimeCounter();
+    clearInterval(interval);
 
     table.timeCounterIsStart = true;
 
-    interval = setInterval(() => {
-        if (timeCounter < 60){
-            seconds = timeCounter;
-            if (seconds < 10){
-                seconds = "0" + seconds;
-            }
-            secondsElement.innerHTML = seconds;
-        } else if (timeCounter >=60 && timeCounter < 3600){
-            seconds = timeCounter % 60;
-            if (seconds < 10){
-                seconds = "0" + seconds;
-            }
-            minutes = Math.floor(timeCounter / 60);
-            if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
-            secondsElement.innerHTML = seconds;
-            minutesElement.innerHTML = minutes;
-        } else if (timeCounter >= 3600){
-            seconds = timeCounter % 60;
-            if (seconds < 10){
-                seconds = "0" + seconds;
-            }
-            minutes = Math.floor(timeCounter / 60 % 60);
-            if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
-            hours = Math.floor(timeCounter / 3600);
-            if (hours < 10) {
-                hours = "0" + hours;
-            }
-            secondsElement.innerHTML = seconds;
-            minutesElement.innerHTML = minutes;
-            hoursElement.innerHTML = hours;
-        }
-        timeCounter++
-    }, 1000);
+    interval = setInterval(() => setTime(++timeCounter), 1000);
 }
 
 export function stopTimer() {
@@ -59,10 +28,5 @@ export function stopTimer() {
 }
 
 export function resetTimeCounter() {
-    let secondsElement = document.getElementById('seconds');
-    let minutesElement = document.getElementById('minutes');
-    let hoursElement = document.getElementById('hours');
-    secondsElement.innerHTML = "00";
-    minutesElement.innerHTML = "00";
-    hoursElement.innerHTML = "00";
+    setTime(timeCounter = 0);
 }
